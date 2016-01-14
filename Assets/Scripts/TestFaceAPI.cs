d16300560667207a4062414d90425844a2e2edfb
Rumen Filkov <rumen.filkov@gmail.com>
2016-01-14 08:07:10 +0100

    initial state of the project

---
+++ Assets/Scripts/TestFaceAPI.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TestFaceAPI : MonoBehaviour 
+{
+	public string url = "http://images.earthcam.com/ec_metros/ourcams/fridays.jpg";
+
+	IEnumerator Start() 
+	{
+		WWW www = new WWW(url);
+		yield return www;
+
+		if(string.IsNullOrEmpty(www.error))
+		{
+			Renderer renderer = GetComponent<Renderer>();
+			renderer.material.mainTexture = www.texture;
+		}
+		else
+		{
+			Debug.LogError(www.error + " - " + www.url);
+		}
+	}
+
+	void Update () 
+	{
+	
+	}
+}
